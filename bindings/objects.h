0d2ba87091891315b87ad1016484523f249918f6
Geequlim <geequlim@gmail.com>
2018-02-01 19:20:02 +0800

    TOOL_ENABLED

--- bindings/objects.h
+++ bindings/objects.h
@@ -1,12 +1,16 @@
 #ifndef AS_BINDING_OBJECT_H
 #define AS_BINDING_OBJECT_H
 
+#include <core/ustring.h>
+
 class asIScriptEngine;
 
 namespace asb {
 
 int declare_object_types(asIScriptEngine* engine);
 int define_object_types(asIScriptEngine* engine);
+
+String get_binding_script_content();
 }
 
 #endif // AS_BINDING_OBJECT_H
